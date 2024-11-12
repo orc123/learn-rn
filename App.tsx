f3ad768e3a48de7d546a6c69cfd2e81ec55888c5
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-12 21:46:07 +0700

    video28

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World Dong.orc12</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>test</Text>
+      </View>
+      <Text style={styles.text}>Hello World
+        <Text style={styles.item}> Dong.orc12</Text>
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: 'red'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 60, 
+    color: 'red'
   },
+  item: {
+    color: 'green'
+  }
 });
