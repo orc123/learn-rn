7c9f167bdd165a27df4eca5b4c5fdc83187fbb1f
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-13 22:17:29 +0700

    video 31

--- App.tsx
+++ App.tsx
@@ -4,6 +4,19 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   const [name, setName] = useState<string>("a");
 
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Learn React Native" },
+    { id: 2, title: "Learn React.js" },
+    { id: 3, title: "Watching Netflix" },
+    { id: 4, title: "Playing ESport" },
+    { id: 5, title: "Subscribe Dong :v" },
+    { id: 6, title: "Watching Youtube" },
+    { id: 7, title: "CR 7" },
+    { id: 8, title: "Tony Kroos" },
+    { id: 9, title: "Nine" },
+    { id: 10, title: "M10" },
+  ]);
+
   return (
     <View style={styles.container}>
       <View>
@@ -20,15 +33,30 @@ export default function App() {
         <Text style={styles.text}>{name}</Text>
       </View>
 
-      <Text style={styles.text}>
-        Hello World
-        <Text style={styles.item}> Dong.orc12</Text>
-      </Text>
+      <Button title="Add new" onPress={() => alert("Tap me")} />
+
+      <View
+        style={{
+          marginTop: 20,
+          borderColor: "red",
+          borderWidth: 1,
+        }}
+      >
+        {todoList.map((todo) => {
+          return <Text style={styles.todo}>{todo.title}</Text>;
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  todo: {
+    fontSize: 30,
+    backgroundColor: "pink",
+    marginBottom: 20,
+    padding: 15,
+  },
   text: {
     fontSize: 30,
     color: "red",
@@ -42,6 +70,7 @@ const styles = StyleSheet.create({
     color: "red",
     paddingTop: 20,
     paddingHorizontal: 20,
+    marginTop: 50,
   },
   item: {
     color: "green",
