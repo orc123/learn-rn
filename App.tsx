b7896018564492ab5fef7ae45f6eb9383a6c05e6
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-16 15:18:02 +0700

    lesson 45

--- App.tsx
+++ App.tsx
@@ -1,55 +1,33 @@
 import { useState } from "react";
-import {
-  Keyboard,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
-import InputTodo from "./components/todo/input.todo";
-import ListTodo from "./components/todo/list.todo";
-import FlexBox from "./components/todo/flexbox";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  const [todoList, setTodoList] = useState<ITodo[]>([]);
+const Stack = createNativeStackNavigator();
 
-  function randomInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const addTodo = (title: string) => {
-    const todo: ITodo = {
-      id: randomInteger(1, 1000000),
-      title: title,
-    };
-    setTodoList([...todoList, todo]);
-  };
-
-  const deleteTodo = (id: number) => {
-    const newTodo = todoList.filter((x) => x.id != id);
-    setTodoList(newTodo);
-  };
-
+function HomeScreen() {
   return (
-    // <FlexBox />
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <InputTodo addTodo={addTodo} />
-        <ListTodo todoList={todoList} deleteTodo={deleteTodo} />
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    fontSize: 60,
-    color: "red",
-    paddingTop: 20,
-    paddingHorizontal: 20,
-    marginTop: 50,
-  },
-});
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
