9b110bcbea118c1dcf6e51d80c3701c02d038d6d
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-19 19:42:54 +0700

    lesson 55

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const AppRoot = () => {
+  return (
+    <View>
+      <Text>1Hello world with Expo Router</Text>
+    </View>
+  );
+};
+export default AppRoot;
