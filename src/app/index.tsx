fa8fe510754cae4a10ef2723c917ee9e3cbbb03d
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-19 21:24:20 +0700

    lesson 56

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 const AppRoot = () => {
   return (
     <View>
       <Text>1Hello world with Expo Router</Text>
+      <Link href={"/orc12"}>Go to Orc12</Link>
+      <Link href={"/like"}>Go to Like</Link>
     </View>
   );
 };
---
+++ src/app/orc12.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const Orc12 = () => {
+  return (
+    <View>
+      <Text>Hello world with Orc12</Text>
+      <Link href={"/"}>Go to home</Link>
+    </View>
+  );
+};
+export default Orc12;
