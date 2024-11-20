5b304914cb67ddd732536f48bf1678370382e98e
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-20 16:03:19 +0700

    video 57

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,23 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { router } from "expo-router";
 
 const AppRoot = () => {
+  const handleLogin = () => {
+    alert("login");
+    //router.navigate("/login");
+    router.navigate("/user");
+  };
   return (
     <View>
       <Text>1Hello world with Expo Router</Text>
       <Link href={"/orc12"}>Go to Orc12</Link>
-      <Link href={"/like"}>Go to Like</Link>
+      <Link href={"/like/like.detail"} asChild>
+        <Button title="Go to like Detail" />
+      </Link>
+      <View style={{ margin: 20 }}>
+        <Button title="Login" onPress={handleLogin} />
+      </View>
     </View>
   );
 };
