29325e7cdadea1aaa72db53bdb535b06684728f2
Dong Pham <ngocdong.pham@wiztechww.com>
2024-11-29 21:45:14 +0700

    lesson 86

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,6 +1,10 @@
+import LoadingOverlay from "@/components/loading/overlay";
+import { verifyCodeAPI } from "@/utils/api";
 import { APP_COLOR } from "@/utils/constant";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, Keyboard } from "react-native";
 import OTPTextView from "react-native-otp-textinput";
+import Toast from "react-native-root-toast";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,32 +19,59 @@ const styles = StyleSheet.create({
 });
 
 const VerifyPage = () => {
+  const [isSubmit, setIsSubmit] = useState<boolean>(false);
+
+  const handleCellTextChange = async (text: string, i: number) => {
+    if (i === 5 && text) {
+      Keyboard.dismiss();
+      setIsSubmit(true);
+      const res = await verifyCodeAPI("admin1@gmail.com", "123456");
+      setIsSubmit(false);
+      if (res.data) {
+        //success
+        alert("success");
+      } else {
+        Toast.show(Array.isArray(res.message) ? res.message[0] : res.message, {
+          duration: Toast.durations.LONG,
+          textColor: "white",
+          backgroundColor: APP_COLOR.ORANGE,
+          opacity: 1,
+        });
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}> Xác thực tài khoản</Text>
-      <Text style={{ marginVertical: 10 }}>
-        Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ hoidanit@gmail.com
-      </Text>
-      <View style={{ marginVertical: 20 }}>
-        <OTPTextView
-          inputCount={6}
-          inputCellLength={1}
-          tintColor={APP_COLOR.ORANGE}
-          textInputStyle={{
-            borderWidth: 1,
-            borderColor: APP_COLOR.GREY,
-            borderBottomWidth: 1,
-            borderRadius: 5,
-            // @ts-ignore:next-line
-            color: APP_COLOR.ORANGE,
-          }}
-        />
+    <>
+      <View style={styles.container}>
+        <Text style={styles.heading}> Xác thực tài khoản</Text>
+        <Text style={{ marginVertical: 10 }}>
+          Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ hoidanit@gmail.com
+        </Text>
+        <View style={{ marginVertical: 20 }}>
+          <OTPTextView
+            autoFocus
+            handleCellTextChange={handleCellTextChange}
+            inputCount={6}
+            inputCellLength={1}
+            tintColor={APP_COLOR.ORANGE}
+            textInputStyle={{
+              borderWidth: 1,
+              borderColor: APP_COLOR.GREY,
+              borderBottomWidth: 1,
+              borderRadius: 5,
+              // @ts-ignore:next-line
+              color: APP_COLOR.ORANGE,
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 10 }}>
+          <Text>Không nhận được mã xác nhận,</Text>
+          <Text style={{ textDecorationLine: "underline" }}> gửi lại</Text>
+        </View>
       </View>
-      <View style={{ flexDirection: "row", marginVertical: 10 }}>
-        <Text>Không nhận được mã xác nhận,</Text>
-        <Text style={{ textDecorationLine: "underline" }}> gửi lại</Text>
-      </View>
-    </View>
+      {isSubmit && <LoadingOverlay />}
+    </>
   );
 };
 export default VerifyPage;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,9 +2,10 @@ import axios from "@/utils/axios.customize";
 
 export const registerAPI = (email: string, password: string, name: string) => {
   const url = `/api/v1/auth/register`;
-  return axios.post<IBackendRes<IRegister>>(url, {
-    email,
-    password,
-    name,
-  });
+  return axios.post<IBackendRes<IRegister>>(url, { email, password, name });
+};
+
+export const verifyCodeAPI = (email: string, code: string) => {
+  const url = `/api/v1/auth/verify-code`;
+  return axios.post<IBackendRes<IRegister>>(url, { email, code });
 };
