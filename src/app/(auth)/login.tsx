777ad843949baf415cf5f9b9d153494343d80f2f
Dong Pham <ngocdong.pham@wiztechww.com>
2024-12-02 20:20:47 +0700

    lesson 97

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -3,10 +3,15 @@ import SocialButton from "@/components/button/social.button";
 import SharedInput from "@/components/input/shared.input";
 import { loginAPI } from "@/utils/api";
 import { APP_COLOR } from "@/utils/constant";
+import { LoginSchema } from "@/utils/validate.schema";
 import { Link, router } from "expo-router";
+import { Formik } from "formik";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  GestureHandlerRootView,
+  TextInput,
+} from "react-native-gesture-handler";
 import Toast from "react-native-root-toast";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -51,7 +56,7 @@ const LoginPage = () => {
   return (
     <GestureHandlerRootView>
       <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
+        {/* <View style={styles.container}>
           <Text style={{ fontSize: 26, fontWeight: 600, marginVertical: 30 }}>
             Đăng nhập
           </Text>
@@ -113,7 +118,41 @@ const LoginPage = () => {
           </View>
 
           <SocialButton title="Đăng nhập với" />
-        </View>
+        </View> */}
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log("check values = ", values)}
+          validationSchema={LoginSchema}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <View style={{ margin: 10 }}>
+              <Text>Email</Text>
+              <TextInput
+                style={{ borderWidth: 1, borderColor: "#ccc" }}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+              {errors.email && (
+                <Text style={{ color: "red" }}>{errors.email}</Text>
+              )}
+              <View style={{ marginVertical: 10 }}></View>
+              <Text>Password</Text>
+              <TextInput
+                style={{ borderWidth: 1, borderColor: "#ccc" }}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+              {errors.password && (
+                <Text style={{ color: "red" }}>{errors.password}</Text>
+              )}
+              <View style={{ marginVertical: 10 }}></View>
+
+              <Button onPress={handleSubmit as any} title="Login" />
+            </View>
+          )}
+        </Formik>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
