989c220ca2a4a96ec33214ad9895ee7a80297953
Dong Pham <ngocdong.pham@wiztechww.com>
2024-12-07 11:55:34 +0700

    lesson 110

---
+++ src/app/(tabs)/notification.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const NotificationPage = () => {
+  return (
+    <View>
+      <Text> notification page</Text>
+    </View>
+  );
+};
+
+export default NotificationPage;
