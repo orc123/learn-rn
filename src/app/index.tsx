6178cecd20fb4a94e56de926f9b305114a6b0447
Dong Pham <ngocdong.pham@wiztechww.com>
2024-12-07 19:55:18 +0700

    lesson 115

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,11 @@
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import ShareButton from "components/button/share,button";
 import { APP_COLOR } from "utils/constant";
 import bg from "@/assets/auth/welcome-background.png";
@@ -7,6 +14,9 @@ import ggLogo from "@/assets/auth/google.png";
 import { LinearGradient } from "expo-linear-gradient";
 import TextBetweenLine from "@/components/button/text.between.line";
 import { Link, Redirect, router } from "expo-router";
+import { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { printAsyncStorage } from "@/utils/api";
 
 const styles = StyleSheet.create({
   container: {
@@ -36,105 +46,120 @@ const styles = StyleSheet.create({
 });
 
 const WelcomePage = () => {
+  useEffect(() => {
+    const test = async () => {
+      await AsyncStorage.setItem("eric", "eric-value");
+      await AsyncStorage.setItem("access_token", "eric-token");
+    };
+    test();
+  }, []);
+
+  const handleClick = async () => {
+    printAsyncStorage();
+  };
+
   // if (true) {
   //   return <Redirect href={"/(tabs)"} />;
   // }
   return (
-    <ImageBackground style={{ flex: 1 }} source={bg}>
-      <LinearGradient
-        style={{ flex: 1 }}
-        colors={["transparent", "#191B2F"]}
-        locations={[0.2, 0.8]}
-      >
-        <View style={styles.container}>
-          <View style={styles.welcomeText}>
-            <Text style={styles.heading}>Welcome to</Text>
-            <Text style={styles.body}>@Orc12 - Food</Text>
-            <Text style={styles.footer}>
-              Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
-            </Text>
-          </View>
-          <View style={styles.welcomeBtn}>
-            <TextBetweenLine title="Đăng nhập với" />
+    <>
+      <Button title="check AsyncStorage" onPress={handleClick} />
+      <ImageBackground style={{ flex: 1 }} source={bg}>
+        <LinearGradient
+          style={{ flex: 1 }}
+          colors={["transparent", "#191B2F"]}
+          locations={[0.2, 0.8]}
+        >
+          <View style={styles.container}>
+            <View style={styles.welcomeText}>
+              <Text style={styles.heading}>Welcome to</Text>
+              <Text style={styles.body}>@Orc12 - Food</Text>
+              <Text style={styles.footer}>
+                Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
+              </Text>
+            </View>
+            <View style={styles.welcomeBtn}>
+              <TextBetweenLine title="Đăng nhập với" />
 
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                gap: 30,
-              }}
-            >
-              <ShareButton
-                title="facebook"
-                onPress={() => {
-                  alert("me");
-                }}
-                textStyle={{ textTransform: "uppercase" }}
-                btnStyle={{
-                  justifyContent: "center",
-                  borderRadius: 30,
-                  backgroundColor: "#fff",
-                }}
-                icons={<Image source={fbLogo} />}
-              />
-              <ShareButton
-                title="google"
-                onPress={() => {
-                  alert("me");
-                }}
-                textStyle={{ textTransform: "uppercase" }}
-                btnStyle={{
-                  justifyContent: "center",
-                  borderRadius: 30,
-                  backgroundColor: "#fff",
-                  paddingHorizontal: 20,
-                }}
-                icons={<Image source={ggLogo} />}
-              />
-            </View>
-            <View>
-              <ShareButton
-                title="Đăng nhập với email"
-                onPress={() => router.navigate("/(auth)/login")}
-                textStyle={{ color: "#fff", paddingVertical: 5 }}
-                btnStyle={{
-                  justifyContent: "center",
-                  borderRadius: 30,
-                  marginHorizontal: 50,
-                  paddingVertical: 10,
-                  backgroundColor: "#2c2c2c",
-                  borderColor: "#505050",
-                  borderWidth: 1,
-                }}
-                pressStyle={{ alignSelf: "stretch" }}
-              />
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                gap: 10,
-                justifyContent: "center",
-              }}
-            >
-              <Text
+              <View
                 style={{
-                  color: "white",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  gap: 30,
                 }}
               >
-                Chưa có tài khoản?
-              </Text>
-              <Link href={"/(auth)/signup"}>
+                <ShareButton
+                  title="facebook"
+                  onPress={() => {
+                    alert("me");
+                  }}
+                  textStyle={{ textTransform: "uppercase" }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    backgroundColor: "#fff",
+                  }}
+                  icons={<Image source={fbLogo} />}
+                />
+                <ShareButton
+                  title="google"
+                  onPress={() => {
+                    alert("me");
+                  }}
+                  textStyle={{ textTransform: "uppercase" }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    backgroundColor: "#fff",
+                    paddingHorizontal: 20,
+                  }}
+                  icons={<Image source={ggLogo} />}
+                />
+              </View>
+              <View>
+                <ShareButton
+                  title="Đăng nhập với email"
+                  onPress={() => router.navigate("/(auth)/login")}
+                  textStyle={{ color: "#fff", paddingVertical: 5 }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    marginHorizontal: 50,
+                    paddingVertical: 10,
+                    backgroundColor: "#2c2c2c",
+                    borderColor: "#505050",
+                    borderWidth: 1,
+                  }}
+                  pressStyle={{ alignSelf: "stretch" }}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  gap: 10,
+                  justifyContent: "center",
+                }}
+              >
                 <Text
-                  style={{ color: "white", textDecorationLine: "underline" }}
+                  style={{
+                    color: "white",
+                  }}
                 >
-                  Đăng ký.
+                  Chưa có tài khoản?
                 </Text>
-              </Link>
+                <Link href={"/(auth)/signup"}>
+                  <Text
+                    style={{ color: "white", textDecorationLine: "underline" }}
+                  >
+                    Đăng ký.
+                  </Text>
+                </Link>
+              </View>
             </View>
           </View>
-        </View>
-      </LinearGradient>
-    </ImageBackground>
+        </LinearGradient>
+      </ImageBackground>
+    </>
   );
 };
 export default WelcomePage;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import axios from "@/utils/axios.customize";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const registerAPI = (email: string, password: string, name: string) => {
   const url = `/api/v1/auth/register`;
@@ -20,5 +21,17 @@ export const loginAPI = (email: string, password: string) => {
   return axios.post<IBackendRes<IUserLogin>>(url, {
     username: email,
     password,
+  });
+};
+
+export const printAsyncStorage = () => {
+  AsyncStorage.getAllKeys((err, keys) => {
+    AsyncStorage.multiGet(keys!, (error, stores) => {
+      let asyncStorage: any = {};
+      stores?.map((result, i, store) => {
+        asyncStorage[store[i][0]] = store[i][1];
+      });
+      console.log(JSON.stringify(asyncStorage, null, 2));
+    });
   });
 };
